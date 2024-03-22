676575ae9fd2445e82787c335ddafe8b63ab2013
Nour Eddine Ait Bouguarri <nait-bou@c3r2p10.1337.ma>
2024-03-22 18:22:57 +0000

    ll

--- client_bonus.c
+++ client_bonus.c
@@ -6,8 +6,43 @@
 /*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 18:01:53 by nait-bou          #+#    #+#             */
-/*   Updated: 2024/03/22 18:02:04 by nait-bou         ###   ########.fr       */
+/*   Updated: 2024/03/22 18:21:07 by nait-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
+
+void	send_message(int pid, char c)
+{
+	int		i;
+	char	s;
+
+	i = 7;
+	while (i >= 0)
+	{
+		s = c >> i & 1;
+		if (s == 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		i--;
+		usleep(100);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	pid;
+
+	i = 0;
+	if (argc != 3)
+	{
+		ft_printf("make sur to provid the PID and the message /n");
+		return (1);
+	}
+	pid = ft_atoi(argv[1]);
+	while (argv[2][i])
+		send_message(pid, argv[2][i++]);
+	send_message(pid, 10);
+}
--- server_bonus.c
+++ server_bonus.c
@@ -6,8 +6,32 @@
 /*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 18:01:04 by nait-bou          #+#    #+#             */
-/*   Updated: 2024/03/22 18:01:35 by nait-bou         ###   ########.fr       */
+/*   Updated: 2024/03/22 18:20:04 by nait-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
+
+char	s = 0;
+
+void	handler(int sig)
+{
+	static int	i = 0;
+	s = s << 1 | (sig == SIGUSR1);
+	i += 1;
+	if (i == 8)
+	{
+		write(1, &s, 1);
+		i = 0;
+	}
+}
+
+int	main(void)
+{
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	ft_printf("the PID of this server is %d\n", getpid());
+	while (1)
+	{
+	}
+}
