3e73b9ca11e238a4172b837a9043c283072aa127
Nour Eddine Ait Bouguarri <nait-bou@c4r9p10.1337.ma>
2024-04-01 11:03:47 +0000

    ll

--- server.c
+++ server.c
@@ -6,28 +6,31 @@
 /*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 11:32:36 by nait-bou          #+#    #+#             */
-/*   Updated: 2024/03/22 17:32:17 by nait-bou         ###   ########.fr       */
+/*   Updated: 2024/04/01 10:23:00 by nait-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-char	s = 0;
+short	*g_s;
 
 void	handler(int sig)
 {
-	static int	i = 0;
-	s = s << 1 | (sig == SIGUSR1);
-	i += 1;
-	if (i == 8)
+	*g_s = *g_s << 1 | (sig == SIGUSR1);
+	*(g_s + 1) += 1;
+	if (*(g_s + 1) == 8)
 	{
-		write(1, &s, 1);
-		i = 0;
+		write(1, (char *)g_s, 1);
+		*(g_s + 1) = 0;
 	}
 }
 
 int	main(void)
 {
+	short	s;
+
+	s = 0;
+	g_s = &s;
 	signal(SIGUSR1, handler);
 	signal(SIGUSR2, handler);
 	ft_printf("the PID of this server is %d\n", getpid());
